111e8893526f142257817fd3f331f3a23cb514f0
Williams Martinez <williams.martinez@utp.ac.pa>
2021-07-19 21:10:45 -0500

    Se hizo la pantalla de login

--- app/src/main/java/com/example/proyectofinal1/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.proyectofinal1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onclik(View view) {
+        switch (view.getId()){
+            case R.id.btnLogear:
+                Toast.makeText(this, "LOGEAR", Toast.LENGTH_SHORT).show();
+                break;
+                
+            case R.id.btnRegistrar:
+                Toast.makeText(this, "REGISTRAR", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
